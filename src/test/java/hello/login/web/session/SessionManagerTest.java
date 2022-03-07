2ff997949beeb87cbf426bba85e28b9ec86ac74a
iokim <iokim1220@gmail.com>
2022-03-07 16:46:27 +0900

    # 로그인 처리하기 - 세션 직접 만들기

---
+++ src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,44 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void sessionTest() {
+
+        // HttpServletResponse는 인터페이스라서 생성할 수 없음, spring이 테스트할 수 있게 mock객체 제공해줌
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        // 세션 생성
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        // 요청에 응답 쿠키 저장
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies()); //mySessionId=44546546
+
+        // 세션 조회
+        Object result = sessionManager.getSession(request);
+        assertThat(result).isEqualTo(member);
+
+        // 세션 만료
+        sessionManager.expire(request);
+        Object expired = sessionManager.getSession(request);
+        assertThat(expired).isNull();
+
+    }
+
+}
